66d54061210b5c0586161a03eb182e93b46772c8
kwame16 <51427903+kwame16@users.noreply.github.com>
2019-09-10 11:21:31 -0400

    Updates

---
+++ precache-manifest.2fa2825b6a9d643e17dcddc29c700a48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4e6f933e18ec0fb837a0d1d0a943ea",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "555d7d69802bf929e5cc",
+    "url": "/clicky/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dfae3aaf776ba70a0d76",
+    "url": "/clicky/static/js/2.eba52fba.chunk.js"
+  },
+  {
+    "revision": "555d7d69802bf929e5cc",
+    "url": "/clicky/static/js/main.b4bc295d.chunk.js"
+  },
+  {
+    "revision": "cb66523332b476d25f8c",
+    "url": "/clicky/static/js/runtime~main.352fb1e4.js"
+  }
+]);
